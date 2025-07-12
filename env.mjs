9ea2ed65ae04540fffe94d17aca43cafce519553
programmersEmperor <mutasim.business2020@gmail.com>
2025-07-12 20:46:52 +0300

    feat: listen to request from 3000

--- env.mjs
+++ env.mjs
@@ -1,15 +1,17 @@
-import { createEnv } from "@t3-oss/env-core"
-import { z } from "zod"
+// import { createEnv } from "@t3-oss/env-core"
+// import { z } from "zod"
 
-export const env = createEnv({
-  server: {
-    GOOGLE_API_KEY: z.string().min(1),
-    OPENAI_API_KEY: z.string().min(1),
-    DASHBOARD_URL: z.string().min(1),
-  },
-  runtimeEnv: {
-    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    DASHBOARD_URL: process.env.DASHBOARD_URL,
-  },
-})
+// export const env = createEnv({
+//   server: {
+//     GREEN_API_INSTANCE_ID: z.string().min(1),
+//     GREEN_API_INSTANCE_TOKEN: z.string().min(1),
+//     OPENAI_API_KEY: z.string().min(1),
+//     PORT: z.number(),
+//   },
+//   runtimeEnv: {
+//     GREEN_API_INSTANCE_ID: process.env.GREEN_API_INSTANCE_ID,
+//     GREEN_API_INSTANCE_TOKEN: process.env.GREEN_API_INSTANCE_TOKEN,
+//     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+//     PORT: process.env.PORT
+//   },
+// })
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,52 @@
 import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import whatsAppClient from "@green-api/whatsapp-api-client";
+import bodyParser from "body-parser";
+
+dotenv.config();
+const PORT: string = process.env.PORT!;
+const GREEN_API_INSTANCE_ID: string = process.env.GREEN_API_INSTANCE_ID!;
+const GREEN_API_INSTANCE_TOKEN: string = process.env.GREEN_API_INSTANCE_TOKEN!;
 
 const app: Application = express();
-const port: number = 3000;
+app.use(bodyParser.json());
+
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Express with TypeScript!');
+    res.send('Hello from Express with TypeScript!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+const restAPI = whatsAppClient.restAPI({
+    idInstance: GREEN_API_INSTANCE_ID,
+    apiTokenInstance: GREEN_API_INSTANCE_TOKEN,
+});
+
+// @ts-ignore
+const webHookAPI = whatsAppClient.webhookAPI(app, "/webhooks");
+webHookAPI.onIncomingMessageText( async (data, idInstance, idMessage, sender, typeMessage, textMessage) => {
+        console.log('START=============================================')
+        console.log(`data ${data.toString()}`);
+        console.log(`idInstance ${idInstance.toString()}`);
+        console.log(`idMessage ${idMessage.toString()}`);
+        console.log(`sender ${sender.toString()}`);
+        console.log(`typeMessage ${typeMessage.toString()}`);
+        console.log(`textMessage ${textMessage.toString()}`);
+        console.log('END=============================================')
+    }
+);
+
+app.listen(PORT, async () => {
+  try {
+    console.log(`Server running on http://localhost:${PORT}`);
+    
+        // Send test message that triggers webhook
+        const response = await restAPI.message.sendMessage(
+            `967${'780884775'}@c.us`,
+            null,
+            "Mutasim Bot is running"
+        );
+  }
+  catch(e){
+    console.error('error: ', e);
+  }
 });
